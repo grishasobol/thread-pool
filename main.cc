17b6c9151edb18a5ba19f787e1aaad0ea0902c4f
Gregory Sobol <grishasobol@mail.ru>
2021-08-07 12:15:30 +0300

    makes as template class

--- main.cc
+++ main.cc
@@ -11,23 +11,43 @@
 using namespace std;
 
 template<class T, class R>
+class ThreadPool
+{
+    static_assert( !is_same<T, void>::value, "Does not support void input type" );
+    static_assert( !is_same<R, void>::value, "Does not support void return type" );
+
+private:
+    struct PoolContext
+    {
+        sem_t sem;
+        map<int, R> results_map;
+        stack<int> threads_idx_stack;
+        mutex data_mutex;
+    };
+
+    static void run_one_thread( const T& arg, R(*func)(T), PoolContext *ctx, int idx);
+
+public:
+    ThreadPool() = delete;
+    static vector<R> run_in_thread_pool( const vector<T>& args, R(*func)(T), int max_treads_num, int threshold);
+};
+
+template<class T, class R>
 void
-run_one_thread( T arg, R(*func)(T), sem_t *sem, map<int, R> *results_map, mutex *results_map_mutex, stack<int> *thread_idx, int idx)
+ThreadPool<T, R>::run_one_thread( const T& arg, R(*func)(T), PoolContext *ctx, int idx)
 {
     R res = func( arg);
-    results_map_mutex->lock();
-    results_map->insert( {idx, res});
-    thread_idx->push( idx);
-    results_map_mutex->unlock();
-    sem_post( sem);
+    ctx->data_mutex.lock();
+    ctx->results_map.insert( {idx, res});
+    ctx->threads_idx_stack.push( idx);
+    ctx->data_mutex.unlock();
+    sem_post( &ctx->sem);
 }
 
 template<class T, class R>
 vector<R>
-run_in_thread_pool( vector<T> args, R(*func)(T), int max_treads_num, int threshold)
+ThreadPool<T, R>::run_in_thread_pool( const vector<T>& args, R(*func)(T), int max_treads_num, int threshold)
 {
-    static_assert( !is_same<R, void>::value, "Does not support void return type" );
-    static_assert( !is_same<T, void>::value, "Does not support void input type" );
     assert( max_treads_num > 0 && "Must have at least one thread" );
     assert( threshold >= 0 && "Cannot be negative" );
 
@@ -41,44 +61,44 @@ run_in_thread_pool( vector<T> args, R(*func)(T), int max_treads_num, int thresho
         return results;
     }
 
-    sem_t sem;
-    map<int, R> results_map;
-    mutex m;
+    PoolContext ctx;
     map<int, thread*> threads_map;
-    stack<int> threads_idx_stack;
 
-    int res = sem_init( &sem, 0, max_treads_num);
+    int res = sem_init( &ctx.sem, 0, max_treads_num);
     assert( res == 0 && "Could not create semaphore" );
 
     int idx = 0;
     for ( auto arg : args )
     {
-        sem_wait( &sem);
-        m.lock();
-        if ( threads_idx_stack.size() > 0 )
+        sem_wait( &ctx.sem);
+
+        thread *thr = nullptr;
+        ctx.data_mutex.lock();
+        if ( ctx.threads_idx_stack.size() > 0 )
         {
-            int idx = threads_idx_stack.top();
-            threads_idx_stack.pop();
-            auto t = threads_map.at( idx);
+            int idx = ctx.threads_idx_stack.top();
+            ctx.threads_idx_stack.pop();
+            thr = threads_map.at( idx);
             threads_map.erase( idx);
-            // TODO:
-            t->join();
         }
-        m.unlock();
-        auto t = new thread( run_one_thread<T, R>, arg, func, &sem, &results_map, &m, &threads_idx_stack, idx);
-        threads_map.insert( {idx, t});
+        ctx.data_mutex.unlock();
+        if ( thr ) thr->join();
+
+        thread *new_thr = new thread( run_one_thread, arg, func, &ctx, idx);
+        threads_map.insert( {idx, new_thr});
+
         idx++;
     }
 
-    for ( auto t : threads_map )
+    for ( auto thr : threads_map )
     {
-        t.second->join();
+        thr.second->join();
     }
 
-    sem_destroy( &sem);
+    sem_destroy( &ctx.sem);
 
     vector<R> res_v;
-    for ( auto res : results_map )
+    for ( auto res : ctx.results_map )
     {
         res_v.push_back( res.second);
     }
@@ -97,7 +117,7 @@ int main()
     vector<int> v;
     for ( int i = 0; i < 1000; i++ ) v.push_back( i);
 
-    auto res1 = run_in_thread_pool<int, int>( v, square, 10, 2);
+    auto res1 = ThreadPool<int, int>::run_in_thread_pool( v, square, 10, 2);
     for ( auto x : res1 ) cout << x << ' ';
     cout << endl;
 }
